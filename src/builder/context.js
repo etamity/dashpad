49ee2e59066d4c8c7355360993cbf9467092fd6a
Etamity <tmi290@meuuknot068160.cof.ds.capitalone.com>
2019-03-05 15:26:12 +0000

    Added copyToClipboard function

--- src/builder/context.js
+++ src/builder/context.js
@@ -31,6 +31,28 @@ const kill = pid => {
     ProcessManager.kill(pid);
 };
 
+
+const copyToClipboard = (text) => {
+    if (window.clipboardData && window.clipboardData.setData) {
+        // IE specific code path to prevent textarea being shown while dialog is visible.
+        return window.clipboardData.setData("Text", text);
+    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        let textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in MS Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy"); // Security exception may be thrown by some browsers.
+        } catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        } finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
+
 /**
  * Global Dashpad api for yaml frontend
  */
@@ -63,6 +85,7 @@ const Dashpad = {
     },
     shell,
     showModal: modal => AppAction.showModal(modal),
+    copyToClipboard,
 };
 
 vm.addGlobal('Dashpad', Dashpad);
