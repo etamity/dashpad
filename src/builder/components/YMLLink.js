9a2ea98f327476d8113c90d864bb27508cfdf2b7
Etamity <tmi290@meuuknot068160.cof.ds.capitalone.com>
2019-03-06 15:26:01 +0000

    Updated link component

--- src/builder/components/YMLLink.js
+++ src/builder/components/YMLLink.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import { shell } from 'electron';
-import { Button, FormGroup } from 'reactstrap';
+import { FormGroup } from 'reactstrap';
+import { YMLButtonView } from './index';
 
 export class YMLLinkView extends Component {
     render() {
-        const { keyPath, obj } = this.props;
-        return (<FormGroup key={keyPath}>
-            <Button
-                className={`text-${obj.color}`}
-                onClick={()=>{
-                    obj.link && shell.openExternal(obj.link)
-                }}
-                color="link"
-            >
-                {obj.icon && <i className={obj.icon} />}
-                {obj.label}
-            </Button></FormGroup>
+        const { name, type, keyPath, obj } = this.props;
+        const props = Object.assign({}, obj, {
+            color: 'link',
+            onClick: () => {
+                obj.link && shell.openExternal(obj.link);
+            },
+        });
+        let newProps = {
+            name,
+            key: keyPath,
+            keyPath: keyPath,
+            type,
+            obj: props,
+        };
+
+        return (
+            <FormGroup key={keyPath}>
+                <YMLButtonView {...newProps} />
+            </FormGroup>
         );
     }
 }
