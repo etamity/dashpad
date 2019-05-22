c3855b9d7e29233711c27d530be50111b392abc1
Etamity <joey.etamity@capitalone.com>
2019-05-22 18:53:46 +0100

    Fixed Dynamic loading module with babel

--- backend/app/module_loader.js
+++ backend/app/module_loader.js
@@ -3,8 +3,7 @@ const { ProcessEventType } = require('./constants');
 const DashpadApi = require('./context/dashpad_api');
 const BackendStore = require('./store');
 const module_path = process.argv.slice(2)[0];
-// console.log(module_path);
-const script = require('/Users/tmi290/documents/dashpad/packages/etamity/data-champion/es6.js').default;
+const script = require(module_path).default;
 process.on('message', action => {
     if (!!action && (typeof action === 'object')) {
         const { type, payload } = action;
