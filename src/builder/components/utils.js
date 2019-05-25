1f4a3e474dde37b7cd4f93298661cf0fa3fad59f
joey.etamity <etamity@gmail.com>
2019-05-25 13:36:17 +0100

    clean up some old code

--- src/builder/components/utils.js
+++ src/builder/components/utils.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 import VM from 'libs/VM';
-import KeyPathManager from 'libs/KeyPathManager';
 import Context from './context';
 import { AppAction } from 'reducers/app';
 import { FieldType, ContentType, UIEvent } from './Constants';
@@ -53,59 +52,6 @@ export const PropsFilter = (props, filters) => {
     });
 };
 
-const getVarsValue = val => {
-    const keyName = val.slice(val.indexOf('${') + 2, val.indexOf('}'));
-    return _.get(Store.getState().app.uiSchema, `$vars.${keyName}`);
-};
-
-const addVarsToKeyPathManager = (val, key, keyPath, name, obj) => {
-    if (_.isString(val) && val.indexOf('${') > -1) {
-        const keyName = val.slice(val.indexOf('${') + 2, val.indexOf('}'));
-        let defaultVal = getVarsValue(val);
-        if (!obj.refs) {
-            obj.refs = {};
-        }
-        obj.refs[key] = keyName;
-        KeyPathManager.push(keyName, `${keyPath}.${name}.${key}`);
-        if (key.indexOf('.') > -1) {
-            return defaultVal;
-        }
-        obj[key] = defaultVal;
-    } else if (_.isArray(val)) {
-        const items = val.map((iValue, index) => {
-            if (!!key && _.isString(iValue) && iValue.indexOf('${') > -1) {
-                const keyName = iValue.slice(
-                    iValue.indexOf('${') + 2,
-                    iValue.indexOf('}')
-                );
-                if (!obj.refs) {
-                    obj.refs = {};
-                }
-                obj.refs[`${key}.${index}`] = keyName;
-                KeyPathManager.push(
-                    keyName,
-                    `${keyPath}.${name}.${key}.${index}`
-                );
-                let defaultVal = getVarsValue(iValue);
-                return defaultVal;
-            } else if (_.isArray(iValue)) {
-                return iValue.map((subVal, subIndex) => {
-                    return addVarsToKeyPathManager(
-                        subVal,
-                        `${key}.${index}.${subIndex}`,
-                        keyPath,
-                        name,
-                        obj
-                    );
-                });
-            }
-            return iValue;
-        });
-        obj[key] = items;
-    }
-    return val;
-};
-
 export const ParseKeyPathVars = (keyPath, obj) => {
     const vars = _.get(Store.getState().app.uiSchema, '$vars');
     return ValueResolver(
@@ -113,38 +59,8 @@ export const ParseKeyPathVars = (keyPath, obj) => {
         '\\${',
         '}',
         vars,
-        keyPath,
-        // (source, keyNames, keyPath, replacedVal) => {
-        //     keyNames.forEach(keyName => {
-        //         KeyPathManager.push(keyName, keyPath);
-        //         const key = keyPath.split('.').pop();
-        //         if (!obj.refs) {
-        //             obj.refs = {};
-        //         }
-        //         obj.refs[key] = keyName;
-        //         obj[key] = replacedVal;
-        //         console.log(
-        //             key,
-        //             'keyName:',
-        //             keyName,
-        //             'keyPath:',
-        //             keyPath,
-        //             'val:',
-        //             replacedVal
-        //         );
-        //     });
-        // }
+        keyPath
     );
-    // _.forEach(obj, (val, key) => {
-    //     if (
-    //         !!val &&
-    //         !_.isEmpty(val) &&
-    //         !Object.values(UIEvent).some(val => key === val)
-    //     ) {
-    //         addVarsToKeyPathManager(val, key, keyPath, name, obj);
-
-    //     }
-    // });
 };
 
 export const EventsHook = (props, events) => {
@@ -158,20 +74,6 @@ export const EventsHook = (props, events) => {
                     keyPath: keyPathFull,
                     value: e.target.value,
                 });
-
-                // const keyRefs = keyPath + '.refs.value';
-                // const keyPathRefs = _.get(
-                //     Store.getState().app.uiSchema,
-                //     keyRefs
-                // );
-
-                // if (keyPathRefs) {
-                //     AppAction.updateUIState({
-                //         keyPath: `$vars.${keyPathRefs}`,
-                //         value: e.target.value,
-                //     });
-                // }
-
             }
             if (
                 next === UIEvent.ON_CLICK &&
