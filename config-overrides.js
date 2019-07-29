d15a25d471a1623500d70207495935a9b80dc176
joey.etamity <etamity@gmail.com>
2019-07-30 00:00:28 +0100

    Update docs config

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,9 @@
+require("util").inspect.defaultOptions.depth = null;
 const path = require('path');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+
+const images = require('remark-images')
+const emoji = require('remark-emoji')
 
 module.exports = {
     webpack: (config, env) => {
@@ -10,24 +14,54 @@ module.exports = {
             config.resolve.plugins = config.resolve.plugins.filter(
                 plugin => !(plugin instanceof ModuleScopePlugin)
             );
-            config.module.rules = [
-                ...config.module.rules,
-                {
-                    test: /\.mdx?$/,
-                    use: ['babel-loader', 'mdx-loader'],
-                    include: [
-                        path.resolve(__dirname, 'src'),
-                        path.resolve(__dirname, 'docs'),
-                    ],
-                },
-            ];
         }
 
         config.module.rules[1].include = [
             path.resolve(__dirname, 'src'),
             path.resolve(__dirname, 'docs'),
         ];
+        config.module.rules = config.module.rules.map(rule => {
+            if (rule.oneOf instanceof Array) {
+                const jsx = rule.oneOf.find(item => {
+                    console.log(item.include, item.include === path.resolve(__dirname, 'docs'));
+                    return item.include && path.resolve(__dirname, 'docs')
+                });
+                jsx.include = [
+                    path.resolve(__dirname, 'src'),
+                    path.resolve(__dirname, 'docs'),
+                ];
+                return {
+                    ...rule,
+                    // create-react-app let every file which doesn't match to any filename test falls back to file-loader,
+                    // so we need to add purs-loader before that fallback.
+                    // see: https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/config/webpack.config.dev.js#L220-L236
+                    oneOf: [
+                        {
+                            test: /\.mdx?$/,
+                            use: [
+                                'babel-loader',
+                                {
+                                  loader: '@mdx-js/loader',
+                                  options: {
+                                    remarkPlugins: [images, emoji]
+                                  }
+                                }
+                              ],
+                            include: [
+                                path.resolve(__dirname, 'docs'),
+                            ],
+                        },
+                        ...rule.oneOf,
+                        jsx
+                    ],
+                };
+            }
+
+            return rule;
+        });
         config.node = { fs: 'empty' };
+        // console.log(config.module);
+        // throw Error();
         return config;
     },
     paths: paths => {
@@ -36,6 +70,7 @@ module.exports = {
         if (process.env.ENTRY_POINT) {
             paths.appSrc = path.resolve(__dirname, 'docs');
             paths.appIndexJs = path.resolve(__dirname, 'docs/index.js');
+            paths.appBuild = path.resolve(__dirname, 'docs/build');
             // console.log(paths);
             // throw Error();
         }
--- docs/App.js
+++ docs/App.js
@@ -1,13 +1,32 @@
-import React, { lazy, Component, Suspense } from 'react';
-import Document, { frontMatter } from './pages/index.mdx';
+import React, { Component, Suspense } from 'react';
+import Document from './pages/index.mdx';
+import Loadable from 'react-loadable';
+import '../src/App.scss';
+// const loading = () => (
+//     <div className="animated fadeIn pt-3 text-center">Loading...</div>
+// );
 
+// const DefaultLayout = Loadable({
+//     loader: () => import('../src/containers/DefaultLayout'),
+//     loading,
+// });
+const routes = [
+    {
+        name: 'Settings',
+        url: '/dashboard/settingsview',
+        icon: 'icon-settings',
+        badge: {
+            variant: 'info',
+            text: 'NEW',
+        }
+    }
+]
 class App extends Component {
     render() {
         return (
             <div>
                 <Suspense fallback={<div>Loading...</div>}>
                     <div>
-                        <h1>{frontMatter.title}</h1>
                         <Document />
                     </div>
                 </Suspense>
