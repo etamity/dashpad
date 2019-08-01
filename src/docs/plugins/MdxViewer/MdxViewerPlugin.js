3513865c2373abd9c5f38708fcb79512500c917a
joey.etamity <etamity@gmail.com>
2019-08-01 02:05:52 +0100

    Update

--- src/docs/plugins/MdxViewer/MdxViewerPlugin.js
+++ src/docs/plugins/MdxViewer/MdxViewerPlugin.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import SideMenus from './Menus/SideMenus'
-export class MdxViewerPlugin extends Component {
+import { Container, Card, CardBody } from 'reactstrap';
+import SideMenus from './Menus/SideMenus';
+import Document, { metadata } from '../../markdowns/index.mdx';
+import { MDXProvider } from '@mdx-js/react';
+import { YMLBuilder } from 'builder/YMLBuilder';
+import yaml from 'js-yaml';
 
-    static Config(){
+const components = {
+    wrapper: (props) => {
+        const { children } = props;
+        return children.map((child, index) => {
+            console.log(child);
+            const { mdxType } = child.props;
+            if (mdxType === 'pre') {
+                const { mdxType, className, ui, children } = child.props.children.props;
+                if (mdxType === 'code' && ['language-yml', 'language-yaml'].includes(className) && ui ) {
+                    return <YMLBuilder key={`uicontainer-${index}`} schema={yaml.safeLoad(children)}></YMLBuilder>
+                }
+            }
+            return child;
+        });
+    },
+};
+
+export class MdxViewerPlugin extends Component {
+    static Config() {
         return {
             SideMenus,
             SubRoutes: [],
@@ -11,6 +32,16 @@ export class MdxViewerPlugin extends Component {
         };
     }
     render() {
-        return <Container className="animated mw-100 fadeIn">SamplePlugin Plugin</Container>
+        return (
+            <Container fluid>
+                <Card>
+                    <CardBody>
+                        <MDXProvider components={components}>
+                            <Document />
+                        </MDXProvider>
+                    </CardBody>
+                </Card>
+            </Container>
+        );
     }
 }
