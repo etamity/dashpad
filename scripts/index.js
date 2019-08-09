1c8a5c15826bdc242a2ac7749e39c3967469191f
Etamity <joey.etamity@capitalone.com>
2019-08-09 15:56:23 +0100

    add update script

--- scripts/index.js
+++ scripts/index.js
@@ -9,17 +9,8 @@ if (PackageJson.version !== version) {
         Config.set('version', PackageJson.version);
         Config.set('project.root', defaultConfig.config.project.root);
     } else {
-        switch (version) {
-            case '1.0.7':
-                const config = Config.value();
-                const password = _.get(config, 'settings.credential.password');
-                const base64Password = Buffer.from(password).toString('base64');
-                _.set(config, 'settings.credential.password', base64Password);
-                _.set(config, 'version', PackageJson.version);
-                Config.saveToFile({ config });
-                break;
-            default:
-                Config.set('version', PackageJson.version);
-        }
+        // TODO
     }
 }
+
+console.log('Dashpad is up to dated!');
