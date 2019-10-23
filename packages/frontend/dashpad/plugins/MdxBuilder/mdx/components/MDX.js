2097ebef4a235ca2d6cf2adf606aac2e66382932
Etamity <joey.etamity@capitalone.com>
2019-10-23 17:41:33 +0100

    Update

---
+++ packages/frontend/dashpad/plugins/MdxBuilder/mdx/components/MDX.js
@@ -0,0 +1,50 @@
+// Direct copy-paste from https://github.com/mdx-js/mdx/releases/tag/v0.20.4
+// + fix for object spreading
+import React from 'react';
+import { transform } from 'buble';
+import mdx from '@mdx-js/mdx';
+import { MDXProvider, mdx as createElement } from '@mdx-js/react';
+
+export default ({
+    scope = {},
+    components = {},
+    remarkPlugins = [],
+    rehypePlugins = [],
+    children,
+    ...props
+}) => {
+    const fullScope = {
+        mdx: createElement,
+        MDXProvider,
+        components,
+        props,
+        ...scope,
+    };
+
+    const jsx = mdx
+        .sync(children, {
+            remarkPlugins,
+            rehypePlugins,
+            skipExport: true,
+        })
+        .trim();
+
+    const { code } = transform(jsx, {
+        objectAssign: 'Object.assign',
+    });
+
+    const keys = Object.keys(fullScope);
+    const values = Object.values(fullScope);
+    // eslint-disable-next-line no-new-func
+    const fn = new Function(
+        '_fn',
+        'React',
+        ...keys,
+        `${code}
+return React.createElement(MDXProvider, { components },
+            React.createElement(MDXContent, props)
+          );`
+    );
+
+    return fn({}, React, ...values);
+};
