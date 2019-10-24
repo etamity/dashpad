496daa7bdb4d4a2236370003834ea215ed9ef743
joey.etamity <etamity@gmail.com>
2019-10-24 04:00:11 +0100

    update

--- packages/frontend/common/builder/components/context.js
+++ packages/frontend/common/builder/components/context.js
@@ -62,7 +62,7 @@ export default props => {
     };
 
     return {
-        ...props,
+        props,
         set,
         get,
         getParent,
--- packages/frontend/common/libs/VM.js
+++ packages/frontend/common/libs/VM.js
@@ -1,7 +1,7 @@
 const compiler = require('@nx-js/compiler-util');
 
 const AllowedScopes =
-    'Dashpad,setInterval,setTimeout,fetch,FileReader,require,DOMParser,Promise,setInterval,console,Array,ArrayBuffer,Boolean,Date,DateTimeFormat,decodeURI,decodeURIComponent,encodeURI,encodeURIComponent,Error,EvalError,Float32Array,Float64Array,Function,Infinity,Intl,Int16Array,Int32Array,Int8Array,isFinite,isNaN,Iterator,JSON,Math,NaN,Number,NumberFormat,Object,parseFloat,parseInt,RangeError,ReferenceError,RegExp,StopIteration,String,SyntaxError,TypeError,Uint16Array,Uint32Array,Uint8Array,Uint8ClampedArray,uneval,URIError,document';
+    'setInterval,setTimeout,fetch,FileReader,require,DOMParser,Promise,setInterval,console,Array,ArrayBuffer,Boolean,Date,DateTimeFormat,decodeURI,decodeURIComponent,encodeURI,encodeURIComponent,Error,EvalError,Float32Array,Float64Array,Function,Infinity,Intl,Int16Array,Int32Array,Int8Array,isFinite,isNaN,Iterator,JSON,Math,NaN,Number,NumberFormat,Object,parseFloat,parseInt,RangeError,ReferenceError,RegExp,StopIteration,String,SyntaxError,TypeError,Uint16Array,Uint32Array,Uint8Array,Uint8ClampedArray,uneval,URIError,document';
 
 var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/gm;
 var ARGUMENT_NAMES = /([^\s,]+)/g;
@@ -9,7 +9,7 @@ var ARGUMENT_NAMES = /([^\s,]+)/g;
 class VM {
     constructor(context) {
         this.context = context || {};
-        this.globals = this.context.globals || [];
+        this.globals = [];
         this.preloadCode = [];
         compiler.expose(...AllowedScopes.split(','));
     }
@@ -18,7 +18,7 @@ class VM {
         this.preloadCode = [];
         this.addCode(code);
         let compileredCode = compiler.compileCode(this.getCode());
-        compileredCode.call(null, { });
+        compileredCode({...this.context}, { ...this.globals });
     }
     getCode() {
         return this.preloadCode.join(';\n');
@@ -32,7 +32,7 @@ class VM {
         const funcbody = this.extractFuncbody(code);
         const allCode = [this.getCode(), funcbody].join('\n');
         let compileredCode = compiler.compileCode(allCode);
-        compileredCode.call(ctx, { [argNames]: args });
+        compileredCode.call({...ctx, ...this.context}, { [argNames]: args, ...this.globals });
     }
     getParamNames(func) {
         const fnStr = func.toString().replace(STRIP_COMMENTS, '');
@@ -55,9 +55,6 @@ class VM {
     }
     deleteGlobal(name) {
         delete this.globals[name];
-    }
-    addAllowGlobal(name) {
-        AllowedScopes.push(name);
     }
     getGlobal(name) {
         return this.globals[name];
